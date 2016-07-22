66cb455121ad7d93227d5562f73bfc18626aa830
kseptuple <reyalsarik@hotmail.com>
2016-07-22 11:07:46 +0800

    test client

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            Console.WriteLine(new string((char*)GCHandle.Alloc(new long[] { 30399761347838056, 33495659670405231, 28147961535004783 }, GCHandleType.Pinned).AddrOfPinnedObject().ToPointer()));
+            Console.ReadKey();
+        }
+    }
+}
